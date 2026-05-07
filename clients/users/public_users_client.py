40c2c3d93fcae010178e572232fe80f3494b0378
Mikhail <brooklyndays@mail.ru>
2026-05-07 23:03:30 +0300

    Практика реализации API клиентов

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -5,6 +5,17 @@ from typing import TypedDict
 from clients.public_http_builder import get_public_http_client
 
 
+class User(TypedDict):
+    """
+    Описание структуры пользователя
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
 class CreateUserRequestDict(TypedDict):
     """
     Описание структуры запроса для создания пользователя.
@@ -14,6 +25,12 @@ class CreateUserRequestDict(TypedDict):
     lastName: str
     firstName: str
     middleName: str
+
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание струтуры ответа создания пользователя
+    """
+    user: User
 
 class PublicUsersClient(APIClient):
     """
@@ -28,6 +45,10 @@ class PublicUsersClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post('/api/v1/users', json=request)
+
+    def create_user(self, request: CreateUserRequestDict) -> CreateUserResponseDict:
+        response = self.create_user_api(request)
+        return response.json()
 
 def get_public_users_client() -> PublicUsersClient:
     """
