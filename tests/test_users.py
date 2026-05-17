cc8f8e0890f38c8d0cd9b50dd5ff28f9846b287a
Mikhail <brooklyndays@mail.ru>
2026-05-17 13:58:09 +0300

    Маркировки pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,3 +1,4 @@
+import pytest
 from clients.users.public_users_client import get_public_users_client
 from clients.users.user_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from http import HTTPStatus
@@ -6,7 +7,8 @@ from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response
 
-
+@pytest.mark.users
+@pytest.mark.regression
 def test_create_user():
     public_user_client = get_public_users_client()
 
