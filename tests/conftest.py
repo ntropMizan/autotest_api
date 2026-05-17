816fd70bf16771d8de0f3f8d5cf25d439ad5ea87
Mikhail <brooklyndays@mail.ru>
2026-05-17 21:03:04 +0300

    Практика написания автотестов с использованием фикстур

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from pydantic import BaseModel, EmailStr
+
 from clients.authentication.authentication_client import get_authentication_client, AuthenticationClient
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import PrivateUsersClient, get_private_users_client
 from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 from clients.users.user_schema import CreateUserRequestSchema, CreateUserResponseSchema
 
@@ -8,6 +11,7 @@ from clients.users.user_schema import CreateUserRequestSchema, CreateUserRespons
 class UserFixture(BaseModel):
     request: CreateUserRequestSchema
     response: CreateUserResponseSchema
+    authentication_user: AuthenticationUserSchema
 
     @property
     def email(self) -> EmailStr:
@@ -33,4 +37,18 @@ def public_users_client() -> PublicUsersClient:
 def function_user(public_users_client: PublicUsersClient) -> UserFixture:
     request = CreateUserRequestSchema()
     response =  public_users_client.create_user(request)
-    return UserFixture(request=request, response=response)
+    authentication_user = AuthenticationUserSchema(
+        email=request.email,
+        password=request.password
+    )
+
+    return UserFixture(
+        request=request,
+        response=response,
+        authentication_user=authentication_user
+    )
+
+
+@pytest.fixture
+def private_users_client(function_user: UserFixture) -> PrivateUsersClient:
+    return get_private_users_client(function_user.authentication_user)
